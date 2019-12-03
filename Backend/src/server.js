d584115aa8498ef4ae66ccdde659112a2a945bbb
silviane1 <silvsbritos@gmail.com>
2019-12-02 22:28:29 -0300

    subindo o mobile e parando nos 7 min

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,19 +3,33 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require ('socket.io');
+const http = require('http');
 
 const routes= require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
-mongoose.connect('mongodb+srv://progweb:progweb@cluster0-hiolf.gcp.mongodb.net/semana09?retryWrites=true&w=majority',{
+io.on('connection', socket =>{
+    console.log('Usuário conectado', socket.id);
+});
+
+mongoose.connect('mongodb+srv://progweb:1234@cluster0-lhqd6.azure.mongodb.net/test?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
+
+// GET, POST, PUT, DELETE
+
+// req.query = ACessr query params (para filtros)
+// req.params = acessar route params (para edição, delete)
+//req.body = acessar corpo da requisição para (criaçãop, edição)
 
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
